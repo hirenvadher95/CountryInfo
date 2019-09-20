09757353892314d11dcb0da4d0418b7ee5652f53
hirenvadher95 <47621081+hirenvadher95@users.noreply.github.com>
2019-09-20 22:59:39 +0530

    Bug fixes

--- countriesinfo/lib/home.dart
+++ countriesinfo/lib/home.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:convert';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter/material.dart';
@@ -6,6 +7,9 @@ import 'package:gradient_app_bar/gradient_app_bar.dart';
 import 'detail_page.dart';
 import 'package:countriesinfo/const.dart';
 import 'package:countriesinfo/privacy.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:connectivity/connectivity.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
 
 List<String> countryName = List<String>();
 List<String> flag = List<String>();
@@ -23,11 +27,34 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   var url = 'https://restcountries.eu/rest/v2/all';
+  var _connectionStatus = 'Unknown';
+  Connectivity connectivity;
+  StreamSubscription<ConnectivityResult> subscription;
 
   @override
   void initState() {
     super.initState();
-    getData();
+    connectivity = Connectivity();
+    subscription =
+        connectivity.onConnectivityChanged.listen((ConnectivityResult result) {
+      _connectionStatus = result.toString();
+      if (result == ConnectivityResult.wifi ||
+          result == ConnectivityResult.mobile) {
+        getData();
+      } else if (result == ConnectivityResult.none) {
+        Alert(
+                context: context,
+                title: "Error",
+                desc: "Check Your Internet Connection!.")
+            .show();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    subscription.cancel();
+    super.dispose();
   }
 
   Future getData() async {
@@ -39,6 +66,7 @@ class _HomeState extends State<Home> {
         // some of svg flag wont render in flutter so skip it
         if (i != 4 &&
             i != 10 &&
+            i != 19 &&
             i != 31 &&
             i != 32 &&
             i != 39 &&
@@ -110,64 +138,84 @@ class _HomeState extends State<Home> {
           backgroundColorStart: Colors.cyan,
           backgroundColorEnd: Colors.indigo,
         ),
-        body: GridView.builder(
-          itemCount: countryName.isEmpty ? 0 : 228,
-          gridDelegate:
-              SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-          itemBuilder: (context, index) {
-            return Center(
-              child: Card(
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(6)),
-                child: Container(
-                  decoration:
-                      BoxDecoration(borderRadius: BorderRadius.circular(8)),
-                  child: RaisedButton(
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => DetailPage()));
-                      sendIndex(index);
-                    },
-                    disabledColor: Theme.of(context).backgroundColor,
-                    color: Theme.of(context).backgroundColor,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Center(
-                          child: Hero(
-                            tag: 'logo$index',
-                            child: SvgPicture.network(
-                              flag[index],
-                              height: 100,
-                              width: 100,
-                              allowDrawingOutsideViewBox: false,
-                              fit: BoxFit.fill,
-                              alignment: Alignment.center,
-                              placeholderBuilder: (BuildContext context) =>
-                                  new Container(
-                                      padding: const EdgeInsets.all(30.0),
-                                      child: const CircularProgressIndicator()),
+        body: FutureBuilder(
+            future: getData(),
+            builder: (context, snapshot) {
+              if (countryName.isNotEmpty) {
+                return GridView.builder(
+                  itemCount: countryName.isEmpty ? 0 : 227,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2),
+                  itemBuilder: (context, index) {
+                    return Center(
+                      child: Card(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(6)),
+                        child: Container(
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(8)),
+                          child: RaisedButton(
+                            onPressed: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => DetailPage()));
+                              sendIndex(index);
+                              print(index);
+
+                            },
+                            disabledColor: Theme.of(context).backgroundColor,
+                            color: Theme.of(context).backgroundColor,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                Center(
+                                  child: Hero(
+                                    tag: 'logo$index',
+                                    child: SvgPicture.network(
+                                      flag[index],
+                                      height: 100,
+                                      width: 100,
+                                      allowDrawingOutsideViewBox: false,
+                                      fit: BoxFit.fill,
+                                      alignment: Alignment.center,
+                                      placeholderBuilder:
+                                          (BuildContext context) =>
+                                              new Container(
+                                                  padding: const EdgeInsets.all(
+                                                      30.0),
+                                                  child: SpinKitDoubleBounce(
+                                                    color: Colors.blue,
+                                                    size: 50,
+                                                  )),
+                                    ),
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 15,
+                                ),
+                                Text(
+                                  countryName[index],
+                                  textAlign: TextAlign.end,
+                                  softWrap: true,
+                                  textDirection: TextDirection.ltr,
+                                )
+                              ],
                             ),
                           ),
                         ),
-                        SizedBox(
-                          height: 15,
-                        ),
-                        Text(
-                          countryName[index],
-                          textAlign: TextAlign.end,
-                          softWrap: true,
-                          textDirection: TextDirection.ltr,
-                        )
-                      ],
-                    ),
+                      ),
+                    );
+                  },
+                );
+              } else {
+                return Center(
+                  child: SpinKitWave(
+                    color: Colors.redAccent,
+                    size: 75.0,
                   ),
-                ),
-              ),
-            );
-          },
-        ),
+                );
+              }
+            }),
       );
 }
